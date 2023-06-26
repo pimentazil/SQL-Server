bd48e2b00a3e043fc0895f9ccffbdc8d1b90bb06
pimentazil <1matheuspimentel@gmail.com>
2023-06-26 16:54:29 -0300

    subindo oo

---
+++ SQLQuery1.sql
@@ -0,0 +1,10 @@
+insert tabUsuario ( nome, usuario, senha) values
+('Matheus', 'pimentazil', '123456')
+
+insert tabUsuario (nome,usuario) values
+('Patolino', 'oMago')
+
+update tabUsuario set nome = 'Patolindo', senha = '1234567890' where id = 3
+
+select * from tabUsuario
+
